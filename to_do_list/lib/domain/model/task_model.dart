7dd7a0b7cc1492970463156b114b55ce6df8688f
StarKzH <88117244+guilhermeSchiochet@users.noreply.github.com>
2023-03-23 22:50:54 -0300

    Alterado model task

--- to_do_list/lib/domain/model/task_model.dart
+++ to_do_list/lib/domain/model/task_model.dart
@@ -1,14 +1,60 @@
+import 'package:flutter/material.dart';
+
+enum TaskPriority {low, medium, high }
+
+extension TaskPriorityExtension on TaskPriority {
+
+  String toShortString() {
+    switch (this) {
+      case TaskPriority.low:
+        return 'Baixa';
+      case TaskPriority.medium:
+        return 'Média';
+      case TaskPriority.high:
+        return 'Alta';
+      default:
+        return '';
+    }
+  }
+
+  Color get color {
+    switch (this) {
+      case TaskPriority.low:
+        return Colors.green;
+      case TaskPriority.medium:
+        return Colors.orange;
+      case TaskPriority.high:
+        return Colors.red;
+      default:
+        return Colors.grey;
+    }
+  }
+
+  IconData get icon {
+    switch (this) {
+      case TaskPriority.high:
+        return Icons.priority_high;
+      case TaskPriority.medium:
+        return Icons.indeterminate_check_box_outlined;
+      default:
+        return Icons.low_priority;
+    }
+  }
+}
+
 class TaskModel {
   final String id;
   final String title;
   final String description;
   final bool isCompleted;
+  final TaskPriority priority;
 
   TaskModel({
     required this.id,
     required this.title,
     required this.description,
     this.isCompleted = false,
+    required this.priority,
   });
 
   factory TaskModel.fromJson(Map<String, dynamic> json) {
@@ -17,6 +63,7 @@ class TaskModel {
       title: json['title'],
       description: json['description'],
       isCompleted: json['isCompleted'] == 1,
+      priority: TaskPriority.values[json['priority']],
     );
   }
 
@@ -26,22 +73,24 @@ class TaskModel {
       'title': title,
       'description': description,
       'isCompleted': isCompleted ? 1 : 0,
+      'priority': priority.index,
     };
   }
 
-  /// Creates a copy of the task with updated properties.
   /// Cria uma cópia da tarefa com propriedades atualizadas.
   TaskModel copyWith({
     String? id,
     String? title,
     String? description,
     bool? isCompleted,
+    TaskPriority? priority,
   }) {
     return TaskModel(
       id: id ?? this.id,
       title: title ?? this.title,
       description: description ?? this.description,
       isCompleted: isCompleted ?? this.isCompleted,
+      priority: priority ?? this.priority,
     );
   }
 }
