136b51c9c1fd9f98d5854df75fb6ab8662e4e637
StarKzH <88117244+guilhermeSchiochet@users.noreply.github.com>
2023-03-23 22:48:51 -0300

    Adicionado documentação aos metodos de criação do bando local

--- to_do_list/lib/data/providers/database_provider.dart
+++ to_do_list/lib/data/providers/database_provider.dart
@@ -1,23 +1,37 @@
-// ignore: depend_on_referenced_packages
+// ignore_for_file: depend_on_referenced_packages
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
+// DatabaseProvider class to manage database operations
+// Classe DatabaseProvider para gerenciar operações do banco de dados
 class DatabaseProvider {
+  // Singleton instance of the DatabaseProvider
+  // Instância singleton do DatabaseProvider
   static final DatabaseProvider _instance = DatabaseProvider._internal();
 
+  // Factory constructor to return the singleton instance
+  // Construtor factory para retornar a instância singleton
   factory DatabaseProvider() {
     return _instance;
   }
 
+  // Private constructor for singleton pattern
+  // Construtor privado para o padrão singleton
   DatabaseProvider._internal();
 
+  // Nullable database object
+  // Objeto de banco de dados nulo
   Database? _database;
 
+  // Getter for the database object, initializes if not yet created
+  // Getter para o objeto do banco de dados, inicializa se ainda não foi criado
   Future<Database> get database async {
     _database ??= await _initDatabase();
     return _database!;
   }
 
+  // Initializes the database
+  // Inicializa o banco de dados
   Future<Database> _initDatabase() async {
     return openDatabase(
       join(await getDatabasesPath(), 'to_do_list.db'),
@@ -27,7 +41,8 @@ class DatabaseProvider {
             id TEXT PRIMARY KEY,
             title TEXT,
             description TEXT,
-            isCompleted INTEGER
+            isCompleted INTEGER,
+            priority INTEGER
           )
         ''');
       },
--- to_do_list/lib/data/providers/task_provider.dart
+++ to_do_list/lib/data/providers/task_provider.dart
@@ -2,9 +2,15 @@ import 'package:sqflite/sqflite.dart';
 import 'package:to_do_list/domain/model/task_model.dart';
 import 'package:to_do_list/data/providers/database_provider.dart';
 
+// TaskProvider class to manage Task-related database operations
+// Classe TaskProvider para gerenciar operações do banco de dados relacionadas às tarefas
 class TaskProvider {
+  // Instance of the DatabaseProvider
+  // Instância do DatabaseProvider
   final DatabaseProvider _databaseProvider = DatabaseProvider();
 
+  // Add a new task to the database
+  // Adicionar uma nova tarefa ao banco de dados
   Future<int> addTask(TaskModel task) async {
     final db = await _databaseProvider.database;
 
@@ -15,6 +21,8 @@ class TaskProvider {
     );
   }
 
+  // Retrieve all tasks from the database
+  // Obter todas as tarefas do banco de dados
   Future<List<TaskModel>> getAllTasks() async {
     final db = await _databaseProvider.database;
 
@@ -25,6 +33,8 @@ class TaskProvider {
     });
   }
 
+  // Update an existing task in the database
+  // Atualizar uma tarefa existente no banco de dados
   Future<int> updateTask(TaskModel task) async {
     final db = await _databaseProvider.database;
 
@@ -36,6 +46,8 @@ class TaskProvider {
     );
   }
 
+  // Delete a task from the database using its ID
+  // Excluir uma tarefa do banco de dados usando seu ID
   Future<int> deleteTask(String id) async {
     final db = await _databaseProvider.database;
     return await db.delete(
