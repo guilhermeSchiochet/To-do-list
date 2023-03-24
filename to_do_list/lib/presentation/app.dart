324277d8d0af95240b694a57821207e70e69cc18
StarKzH <88117244+guilhermeSchiochet@users.noreply.github.com>
2023-03-23 22:51:16 -0300

    Adicionado instancia a classe app para passar para sua filha.

--- to_do_list/lib/presentation/app.dart
+++ to_do_list/lib/presentation/app.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_list/presentation/screens/home_page.dart';
 import 'package:to_do_list/presentation/widgets/theme_app.dart';
+import 'package:to_do_list/domain/useCases/add_use_case.dart';
+import 'package:to_do_list/domain/useCases/delete_use_case.dart';
+import 'package:to_do_list/domain/useCases/update_use_case.dart';
 
 class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+  final AddTaskUseCase addTaskUseCase;
+  final DeleteTaskUseCase deleteTaskUseCase;
+  final UpdateTaskUseCase updateTaskUseCase;
+
+  const App({
+    Key? key,
+    required this.addTaskUseCase,
+    required this.deleteTaskUseCase,
+    required this.updateTaskUseCase,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: HomePage(
+        addTaskUseCase: addTaskUseCase,
+        deleteTaskUseCase: deleteTaskUseCase,
+        updateTaskUseCase: updateTaskUseCase,
+      ),
       theme: ThemeApp.themeApp,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
