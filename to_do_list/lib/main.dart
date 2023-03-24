d35dac9bcc3b2931f15f973a2358af64dd482fe3
StarKzH <88117244+guilhermeSchiochet@users.noreply.github.com>
2023-03-23 22:48:21 -0300

    Inicializado instancias dos casos de uso

--- to_do_list/lib/main.dart
+++ to_do_list/lib/main.dart
@@ -1,3 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_list/presentation/app.dart';
-void main() => runApp(const App());
+import 'package:to_do_list/data/task_repository.dart';
+import 'package:to_do_list/domain/useCases/add_use_case.dart';
+import 'package:to_do_list/domain/useCases/delete_use_case.dart';
+import 'package:to_do_list/domain/useCases/update_use_case.dart';
+
+void main() {
+
+  // Inicializa os casos de uso
+  final taskRepository = TaskRepository();
+  final addTaskUseCase = AddTaskUseCase(repository: taskRepository);
+  final deleteTaskUseCase = DeleteTaskUseCase(repository: taskRepository);
+  final updateTaskUseCase = UpdateTaskUseCase(repository: taskRepository);
+
+  runApp(
+    App(
+      addTaskUseCase: addTaskUseCase,
+      deleteTaskUseCase: deleteTaskUseCase,
+      updateTaskUseCase: updateTaskUseCase,
+    )
+  );
+}
