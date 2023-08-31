de6e41cdb9382a7702898e704779396821e688db
StarKzH <88117244+guilhermeSchiochet@users.noreply.github.com>
2023-08-30 21:39:22 -0300

    change theme

--- to_do_list/lib/src/config/themes/app_theme.dart
+++ to_do_list/lib/src/config/themes/app_theme.dart
@@ -4,6 +4,7 @@ abstract class AppTheme {
   static ThemeData get light {
     return ThemeData(
       useMaterial3: true,
+      cardColor: Colors.white,
       appBarTheme: const AppBarTheme(
         elevation: 0,
         color: Colors.white,
@@ -12,7 +13,22 @@ abstract class AppTheme {
       primaryColor: Colors.black,
       splashColor: Colors.transparent,
       fontFamily: 'IBM',
-      colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.black),
+      colorScheme: ColorScheme.fromSwatch(
+        primarySwatch:  const MaterialColor(0xFFFFFFFF, <int, Color>{
+          50: Colors.white,
+          100: Colors.white,
+          200: Colors.white,
+          300: Colors.white,
+          400: Colors.white,
+          500: Colors.white,
+          600: Colors.white,
+          700: Colors.white,
+          800: Colors.white,
+          900: Colors.white,
+        }),
+        backgroundColor: Colors.white, // cor de fundo do seu aplicativo
+        errorColor: Colors.red, // cor de erro do seu aplicativo
+      ),
     );
   }
 
