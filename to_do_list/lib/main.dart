06a5be8c10342a280b3d060e77b62cbf5fcc5e43
StarKzH <88117244+guilhermeSchiochet@users.noreply.github.com>
2023-09-13 21:37:05 -0300

    Ajustado documentação

--- to_do_list/lib/main.dart
+++ to_do_list/lib/main.dart
@@ -7,8 +7,9 @@ import 'package:to_do_list/src/domain/useCases/update_use_case.dart';
 
 void main() {
 
-  // Inicializa os casos de uso
   final taskRepository = TaskRepository();
+
+  // Inicializa os casos de uso
   final addTaskUseCase = AddTaskUseCase(repository: taskRepository);
   final deleteTaskUseCase = DeleteTaskUseCase(repository: taskRepository);
   final updateTaskUseCase = UpdateTaskUseCase(repository: taskRepository);
