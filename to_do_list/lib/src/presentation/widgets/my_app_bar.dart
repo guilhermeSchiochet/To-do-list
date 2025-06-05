b36c7c334fe962e23a4f3d4b3ec22837442b0f78
Guilherme Schiochet <gpkguilherme123@gmail.com>
2025-06-04 21:47:22 -0300

    Inicializado adptação ao novo layout estipulado.

--- to_do_list/lib/src/presentation/widgets/my_app_bar.dart
+++ to_do_list/lib/src/presentation/widgets/my_app_bar.dart
@@ -1,41 +1,53 @@
-import 'dart:io';
-import 'package:lottie/lottie.dart'; 
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String? avatarImageUrl;
 
-  const MyAppBar({Key? key, this.avatarImageUrl}) : super(key: key);
+  const MyAppBar({super.key, this.avatarImageUrl});
 
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight * 2.1);
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight * 2.4);
 
   @override
   Widget build(BuildContext context) {
+    final double screenHeight = preferredSize.height;
+
     return Container(
       height: preferredSize.height,
       color: Theme.of(context).appBarTheme.backgroundColor,
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: SafeArea(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Stack(
           children: [
-            Text(
-              'Welcome!',
-              style: TextStyle(
-                fontSize: 26,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey.shade700,
+            Center(
+              child: Row(
+                children: [
+                  Text(
+                    'Welcome back!',
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      color: const Color.fromARGB(255, 65, 63, 63),
+                    ),
+                  ),
+                  const Spacer(),
+                  _buildIconSearch(),
+                  const SizedBox(width: 10),
+                  _buildIconNotification(),
+                ],
               ),
             ),
-            _circleAvatar(
-              context,
-              size: 27,
-              onTapDown: (TapDownDetails details) {
-                _showPopupMenu(details.globalPosition, context);
-              },
+            Positioned(
+              left: 8,
+              top: screenHeight * 0.45,
+              child: Text(
+                '2 tasks pending',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w800,
+                  color: Colors.grey.shade500,
+                ),
+              ),
             ),
           ],
         ),
@@ -43,171 +55,207 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
     );
   }
 
-  void _showPopupMenu(Offset offset, context) async {
-
-    await showMenu(
-      context: context,
-      color: Colors.white,
-      position: const RelativeRect.fromLTRB(110, 50, 90, 20),
-      items: <PopupMenuEntry<String>>[
-        PopupMenuItem<String>(
-            enabled: false,
-            value: 'Desenvolvedor',
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                _circleAvatar(
-                  context,
-                  size: 70,
-                  onTapDown: (c) {
-                    _showModalBottomSheet(context);
-                  },
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                ListTile(
-                  title: const Text(
-                    'Configurações',
-                    textAlign: TextAlign.center,
-                  ),
-                  onTap: () {},
-                ),
-                Divider(
-                    height: 0.5, thickness: 0.5, color: Colors.grey.shade800),
-                ListTile(
-                  title: const Text(
-                    'Desenvolvedor',
-                    textAlign: TextAlign.center,
-                  ),
-                  onTap: () {},
-                ),
-              ],
-            )),
-      ],
-      elevation: 8.0,
-    );
-  }
-
-  void _showModalBottomSheet(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      useSafeArea: true,
-      builder: (BuildContext context) {
-        return Padding(
-          padding: const EdgeInsets.only(top: 15, bottom: 40),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text(
-                  'Selecionar imagem',
-                  textAlign: TextAlign.right,
-                  style: TextStyle(
-                    fontSize: 21,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey.shade800
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      _pickImage(ImageSource.gallery);
-                    },
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Lottie.asset(
-                          'assets/json/gallery_animated.json',
-                          width: 120,
-                          height: 120,
-                          repeat: false,
-                        ),
-                        const Text(
-                          'Galeria',
-                          style: TextStyle(
-                            fontSize: 17, fontWeight: FontWeight.w500
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      _pickImage(ImageSource.camera);
-                    },
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Lottie.asset(
-                          'assets/json/camera_animated.json',
-                          width: 140,
-                          height: 120,
-                          repeat: false,
-                        ),
-                        const Text(
-                          'Câmera',
-                          style: TextStyle(
-                            fontSize: 17,
-                            fontWeight: FontWeight.w500
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ],
+  Widget _buildIconNotification() {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        IconButton(
+          icon: const Icon(
+            Icons.notifications_none_outlined,
+            size: 30,
           ),
-        );
-      },
+          onPressed: () {},
+        ),
+        Positioned(
+          top: 14,
+          right: 14,
+          child: Container(
+            width: 8,
+            height: 8,
+            decoration: const BoxDecoration(
+              color: Colors.red,
+              shape: BoxShape.circle,
+            ),
+          ),
+        ),
+      ],
     );
   }
 
-  Future<void> _pickImage(ImageSource source) async {
-    final picker = ImagePicker();
-    final pickedFile = await picker.pickImage(source: source);
-
-    if (pickedFile != null) {
-      _saveImagePath(pickedFile.path);
-    }
-  }
-
-  Future<void> _saveImagePath(String imagePath) async {
-    try {
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      prefs.setString('avatar_image_path', imagePath);
-    } catch (e) {
-      throw 'Erro ao salvar o caminho da imagem: $e';
-    }
-  }
-
-  Future<String?> _getImagePath() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getString('avatar_image_path');
-  }
-
-  Widget _circleAvatar(context, {required double size, void Function(TapDownDetails)? onTapDown}) {
-    return FutureBuilder(
-      future: _getImagePath(),
-      builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
-        return GestureDetector(
-          onTapDown: onTapDown,
-          child: CircleAvatar(
-            radius: size,
-            backgroundColor: Colors.grey.shade100,
-            backgroundImage: snapshot.data != null ? FileImage(File(snapshot.data!)) : null,
-            child: snapshot.data == null ? const CircularProgressIndicator() : null,
-          )
-        );
-      },
+  Widget _buildIconSearch () {
+    return IconButton(
+      icon: const Icon(
+        Icons.search,
+        size: 30,
+      ),
+      onPressed: () {},
     );
   }
+  
+  // void _showPopupMenu(Offset offset, context) async {
+
+  //   await showMenu(
+  //     context: context,
+  //     color: Colors.white,
+  //     position: const RelativeRect.fromLTRB(110, 50, 90, 20),
+  //     items: <PopupMenuEntry<String>>[
+  //       PopupMenuItem<String>(
+  //           enabled: false,
+  //           value: 'Desenvolvedor',
+  //           child: Column(
+  //             crossAxisAlignment: CrossAxisAlignment.stretch,
+  //             children: [
+  //               _circleAvatar(
+  //                 size: 70,
+  //                 onTapDown: (c) {
+  //                   _showModalBottomSheet(context);
+  //                 },
+  //               ),
+  //               const SizedBox(
+  //                 height: 10,
+  //               ),
+  //               ListTile(
+  //                 title: const Text(
+  //                   'Configurações',
+  //                   textAlign: TextAlign.center,
+  //                 ),
+  //                 onTap: () {},
+  //               ),
+  //               Divider(
+  //                   height: 0.5, thickness: 0.5, color: Colors.grey.shade800),
+  //               ListTile(
+  //                 title: const Text(
+  //                   'Desenvolvedor',
+  //                   textAlign: TextAlign.center,
+  //                 ),
+  //                 onTap: () {},
+  //               ),
+  //             ],
+  //           )),
+  //     ],
+  //     elevation: 8.0,
+  //   );
+  // }
+
+  // void _showModalBottomSheet(BuildContext context) {
+  //   showModalBottomSheet(
+  //     context: context,
+  //     useSafeArea: true,
+  //     builder: (BuildContext context) {
+  //       return Padding(
+  //         padding: const EdgeInsets.only(top: 15, bottom: 40),
+  //         child: Column(
+  //           crossAxisAlignment: CrossAxisAlignment.start,
+  //           mainAxisSize: MainAxisSize.min,
+  //           children: [
+  //             Padding(
+  //               padding: const EdgeInsets.all(20.0),
+  //               child: Text(
+  //                 'Selecionar imagem',
+  //                 textAlign: TextAlign.right,
+  //                 style: TextStyle(
+  //                   fontSize: 21,
+  //                   fontWeight: FontWeight.bold,
+  //                   color: Colors.grey.shade800
+  //                 ),
+  //               ),
+  //             ),
+  //             Row(
+  //               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  //               children: [
+  //                 GestureDetector(
+  //                   onTap: () {
+  //                     _pickImage(ImageSource.gallery);
+  //                   },
+  //                   child: Column(
+  //                     crossAxisAlignment: CrossAxisAlignment.center,
+  //                     mainAxisAlignment: MainAxisAlignment.end,
+  //                     children: [
+  //                       Lottie.asset(
+  //                         'assets/json/gallery_animated.json',
+  //                         width: 120,
+  //                         height: 120,
+  //                         repeat: false,
+  //                       ),
+  //                       const Text(
+  //                         'Galeria',
+  //                         style: TextStyle(
+  //                           fontSize: 17, fontWeight: FontWeight.w500
+  //                         ),
+  //                       )
+  //                     ],
+  //                   ),
+  //                 ),
+  //                 GestureDetector(
+  //                   onTap: () {
+  //                     _pickImage(ImageSource.camera);
+  //                   },
+  //                   child: Column(
+  //                     crossAxisAlignment: CrossAxisAlignment.center,
+  //                     mainAxisAlignment: MainAxisAlignment.end,
+  //                     children: [
+  //                       Lottie.asset(
+  //                         'assets/json/camera_animated.json',
+  //                         width: 140,
+  //                         height: 120,
+  //                         repeat: false,
+  //                       ),
+  //                       const Text(
+  //                         'Câmera',
+  //                         style: TextStyle(
+  //                           fontSize: 17,
+  //                           fontWeight: FontWeight.w500
+  //                         ),
+  //                       )
+  //                     ],
+  //                   ),
+  //                 ),
+  //               ],
+  //             ),
+  //           ],
+  //         ),
+  //       );
+  //     },
+  //   );
+  // }
+
+  // Future<void> _pickImage(ImageSource source) async {
+  //   final picker = ImagePicker();
+  //   final pickedFile = await picker.pickImage(source: source);
+
+  //   if (pickedFile != null) {
+  //     _saveImagePath(pickedFile.path);
+  //   }
+  // }
+
+  // Future<void> _saveImagePath(String imagePath) async {
+  //   try {
+  //     SharedPreferences prefs = await SharedPreferences.getInstance();
+  //     prefs.setString('avatar_image_path', imagePath);
+  //   } catch (e) {
+  //     throw 'Erro ao salvar o caminho da imagem: $e';
+  //   }
+  // }
+
+  // Future<String?> _getImagePath() async {
+  //   SharedPreferences prefs = await SharedPreferences.getInstance();
+  //   return prefs.getString('avatar_image_path');
+  // }
+
+  // Widget _circleAvatar({required double size, void Function(TapDownDetails)? onTapDown}) {
+  //   return FutureBuilder(
+  //     future: _getImagePath(),
+  //     builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
+  //       return GestureDetector(
+  //         onTapDown: onTapDown,
+  //         child: CircleAvatar(
+  //           radius: size,
+  //           backgroundColor: Colors.grey.shade100,
+  //           backgroundImage: snapshot.data != null ? FileImage(File(snapshot.data!)) : null,
+  //           child: snapshot.data == null ? const CircularProgressIndicator() : null,
+  //         )
+  //       );
+  //     },
+  //   );
+  // }
 }
--- to_do_list/lib/src/presentation/widgets/task_list_widgets.dart
+++ to_do_list/lib/src/presentation/widgets/task_list_widgets.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:to_do_list/src/domain/model/task_model.dart';
 import 'package:to_do_list/src/presentation/screens/add_task_screen.dart';
+import 'package:to_do_list/src/utils/extensions/colors_extension.dart';
 import 'package:to_do_list/src/utils/extensions/task_priority_extension.dart';
 
 class TaskListWidget extends StatelessWidget {
@@ -12,59 +12,159 @@ class TaskListWidget extends StatelessWidget {
   final Function(TaskModel)? onDelete;
 
   const TaskListWidget({
-    Key? key,
+    super.key,
     this.onUpdate,
     this.onDelete,
     required this.tasks,
     this.showActions = false,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return ListView.separated(
       itemCount: tasks.length,
       itemBuilder: (context, index) {
         final task = tasks[index];
         return _buildTaskItem(context, task);
       },
+      separatorBuilder: (context, index) => const SizedBox(height: 12),
     );
   }
 
   Widget _buildTaskItem(BuildContext context, TaskModel task) {
-    return Slidable(
-      startActionPane: ActionPane(
-        motion: const ScrollMotion(),
-        extentRatio: 0.5,
-        children: [
-          SlidableAction(
-            onPressed: (context) => onDelete!(task),
-            backgroundColor: const Color(0xFFFE4A49),
-            foregroundColor: Colors.white,
-            icon: Icons.delete,
-            label: 'Delete',
+    return _buildTaskTile(context, task);
+  }
+
+  Widget _buildTaskTile(BuildContext context, TaskModel task) {
+    return SizedBox(
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.grey.shade200,
           ),
-        ],
-      ),
-      endActionPane: showActions ? null : ActionPane(
-        motion: const ScrollMotion(),
-        extentRatio: 0.5,
-        children: [
-          SlidableAction(
-            onPressed: (context) => onUpdate!(task.copyWith(isCompleted: true)),
-            backgroundColor: const Color.fromARGB(255, 16, 235, 107),
-            foregroundColor: Colors.white,
-            icon: Icons.check,
-            label: 'Complete',
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Stack(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Transform.scale(
+                    scale: 1.2,
+                    child: Checkbox(
+                      value: task.isCompleted,
+                      activeColor: Colors.blue,
+                      checkColor: Colors.white,
+                      side: BorderSide(
+                        color: Colors.grey.shade300,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      onChanged: (bool? value) {
+                        onUpdate!(task.copyWith(isCompleted: value!));
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width / 1.5,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                task.title,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey.shade800,
+                                ),
+                              ),
+                              if(task.description != null) Text(
+                                task.description!,
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.grey.shade600,
+                                ),
+                              ),    
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          children: [
+                            _buildInfoStatus(color: Colors.green),
+                            SizedBox(width: 4),
+                            _buildInfoStatus(color: Colors.pink),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Positioned(
+                top: 0,
+                right: 0,
+                child: _buildpriority(
+                  text: task.priority.toShortString(),
+                  color: task.priority.color
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
-      child: _buildTaskTile(context, task),
+    );
+  }
+
+  Widget _buildInfoStatus({required Color color}) {
+    return _baseBuild(
+      padding: EdgeInsetsGeometry.symmetric(horizontal: 10, vertical: 4.0),
+      text: "#Task",
+      color: color.lighten(40),
+      colorText: color
+    );
+  }
+
+  Widget _buildpriority({required Color color, required String text}) {
+    return _baseBuild(
+      padding: EdgeInsetsGeometry.symmetric(horizontal: 10, vertical: 4.0),
+      text: text,
+      color: color,
+    );
+  }
+
+  Widget _baseBuild({required EdgeInsetsGeometry padding, required String text, required Color color, Color colorText = Colors.white}) {
+    return Card(
+      color: color,
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: Colors.transparent),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: padding,
+        child: Text(
+          text,
+          style: TextStyle(
+            color: colorText,
+            fontWeight: FontWeight.bold,
+            fontSize: 12,
+          ),  
+        ),
+      ),
     );
   }
 
   /// Cria um ListTile para a tarefa com ações de clique e segurar.
-  Widget _buildTaskTile(BuildContext context, TaskModel task) {
+  Widget _buildTaskTile2(BuildContext context, TaskModel task) {
     return ListTile(
+      style: ListTileStyle.list,
       title: Row(
         children: [
           _buildPriorityIcon(task),
@@ -75,8 +175,8 @@ class TaskListWidget extends StatelessWidget {
       subtitle: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            task.description,
+          if(task.description != null) Text(
+            task.description!,
             overflow: TextOverflow.ellipsis,
           ),
           const SizedBox(height: 4),
@@ -84,7 +184,7 @@ class TaskListWidget extends StatelessWidget {
         ],
       ),
       leading: task.isCompleted ? CircleAvatar(
-        backgroundColor: Colors.green.withOpacity(0.2),
+        backgroundColor: Colors.green.withValues(alpha: 0.2),
         child: const Align(
           alignment: Alignment.center,
           child: Text(
@@ -126,7 +226,7 @@ class TaskListWidget extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 8),
       decoration: BoxDecoration(
-        color: statusColor.withOpacity(0.2),
+        color: statusColor.withValues(alpha: 0.2),
         borderRadius: BorderRadius.circular(4),
       ),
       child: Text(
