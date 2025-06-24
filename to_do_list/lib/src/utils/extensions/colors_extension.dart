ceebee5e53710beff7a244bc16b4d7d3c2cf9dea
Guilherme Schiochet <gpkguilherme123@gmail.com>
2025-06-23 22:09:11 -0300

    Ajustado para usar o método withValeus ( Novo padrão )
    'withOpacity' is deprecated and shouldn't be used. Use .withValues() to avoid precision loss.
    Try replacing the use of the deprecated member with the replacement.

--- to_do_list/lib/src/utils/extensions/colors_extension.dart
+++ to_do_list/lib/src/utils/extensions/colors_extension.dart
@@ -1,8 +1,7 @@
-import 'dart:math' as math;
 import 'package:flutter/material.dart';
 
 extension CColor on Color {
   Color withAlphaPercent(double percent) {
-    return withOpacity(percent.clamp(0.0, 1.0));
+    return withValues(alpha: percent.clamp(0.0, 1.0));
   }
 }
